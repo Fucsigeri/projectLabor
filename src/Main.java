73a4d17280cc32e387c80e8909861d0e9ad83699
Demeter Dobos <demeter.dobos@gmail.com>
2018-12-08 12:48:53 +0100

    Changesqwe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Sracokkk!");
+        System.out.println("Hello Sracok!");
+        System.out.println("OL");
+
     }
 }
